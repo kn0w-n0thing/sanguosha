5c5a9ba7f036fba39223c680bcca50b235083535
clldaxigua <clldaxigua@126.com>
2020-10-01 16:25:21 +0800

    Add google test, but fail to run the test case.

--- build.sh
+++ build.sh
@@ -4,8 +4,12 @@ CURRENT_DIR=`pwd`
 BUILD_DIR=${CURRENT_DIR}/build
 SRC_DIR=${CURRENT_DIR}/src
 CMAKE_COMMAND=cmake
+MAKE_COMMAND=make
 
 mkdir ${BUILD_DIR}
+mkdir -p ${CURRENT_DIR}/external/gtest
 cd ${BUILD_DIR}
 ${CMAKE_COMMAND} ${SRC_DIR}
+${MAKE_COMMAND}
+
 
